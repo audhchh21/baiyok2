7d6d2325313a5517fa4b1ccbed2586670498870f
Nice <audhchh21@gmail.com>
2020-01-21 03:10:52 +0700

    1

---
+++ resources/views/layouts/navbar/footer.blade.php
@@ -0,0 +1,20 @@
+<footer class="main-footer d-flex p-2 px-3 bg-white border-top">
+    <ul class="nav">
+        <li class="nav-item">
+            <a class="nav-link" href="">{{ __('Home') }}</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="">{{ __('Home') }}Services</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="">{{ __('Home') }}About</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="">{{ __('Home') }}Products</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="">{{ __('Home') }}Blog</a>
+        </li>
+    </ul>
+    <span class="copyright ml-auto my-auto mr-2">{{ __('Copyright © 2018') }}</span>
+</footer>
---
+++ resources/views/layouts/navbar/header.blade.php
@@ -0,0 +1,6 @@
+<div class="page-header row no-gutters py-4">
+    <div class="col-12 col-sm-4 text-center text-sm-left mb-0">
+        <h3 class="page-title">{{ __('Page Title') }}</h3>
+        <span class="text-uppercase page-subtitle">{{ __('Page Subtitle') }}</span>
+    </div>
+</div>
